a8222eb6d774d975c572ef635c819664e50d6f71
roman <mromanek1802@gmail.com>
2022-09-28 20:06:21 +0200

    Change CommentController mappings

--- src/main/java/pl/romanek/blog/controllers/CommentController.java
+++ src/main/java/pl/romanek/blog/controllers/CommentController.java
@@ -1,17 +1,17 @@
 package pl.romanek.blog.controllers;
 
 import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import pl.romanek.blog.entities.Comment;
 import pl.romanek.blog.services.CommentService;
 
 @RestController
+@RequestMapping("/comments")
 public class CommentController {
 
     private final CommentService commentService;
@@ -20,19 +20,19 @@ public class CommentController {
         this.commentService = commentService;
     }
 
-    @GetMapping("/comments")
+    @GetMapping("/all")
     public List<Comment> getAllComments() {
         return commentService.findAllComments();
     }
 
-    @GetMapping("comments/post")
-    public List<Comment> getAllCommentsInPost(@RequestParam("id") Integer id) {
+    @GetMapping("/post/{id}")
+    public List<Comment> getAllCommentsInPost(@PathVariable("id") Integer id) {
         return commentService.findAllCommentsInPost(id);
     }
 
-    @PostMapping("/comment")
-    public void addComment(@RequestBody Comment comment, @RequestParam("userId") Integer userId,
-            @RequestParam("postId") Integer postId) {
+    @PostMapping("/add/{userId}/{postId}")
+    public void addComment(@RequestBody Comment comment, @PathVariable("userId") Integer userId,
+            @PathVariable("postId") Integer postId) {
 
         commentService.addComment(comment, userId, postId);
     }
