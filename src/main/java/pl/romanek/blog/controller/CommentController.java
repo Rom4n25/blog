d9e1e331e80dbeed938b96071922e86672bbeb10
roman <mromanek1802@gmail.com>
2022-09-29 20:36:34 +0200

    Add ResponseEntity

--- src/main/java/pl/romanek/blog/controller/CommentController.java
+++ src/main/java/pl/romanek/blog/controller/CommentController.java
@@ -1,13 +1,15 @@
 package pl.romanek.blog.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import pl.romanek.blog.entity.Comment;
 import pl.romanek.blog.service.CommentService;
 
@@ -22,20 +24,28 @@ public class CommentController {
     }
 
     @GetMapping("/all")
-    public List<Comment> getAllComments() {
-        return commentService.findAllComments();
+    public ResponseEntity<List<Comment>> getAllComments() {
+        List<Comment> comments = new ArrayList<>(commentService.findAllComments());
+        if (comments.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(comments);
     }
 
     @GetMapping("/post/{id}")
-    public List<Comment> getAllCommentsInPost(@PathVariable("id") Integer id) {
-        return commentService.findAllCommentsInPost(id);
+    public ResponseEntity<List<Comment>> getAllCommentsInPost(@PathVariable("id") Integer id) {
+        List<Comment> comments = new ArrayList<>(commentService.findAllCommentsInPost(id));
+        if (comments.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(comments);
     }
 
     @PostMapping("/add/{userId}/{postId}")
-    public void addComment(@RequestBody Comment comment, @PathVariable("userId") Integer userId,
+    public ResponseEntity<Void> addComment(@RequestBody Comment comment, @PathVariable("userId") Integer userId,
             @PathVariable("postId") Integer postId) {
 
         commentService.addComment(comment, userId, postId);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
-
 }
