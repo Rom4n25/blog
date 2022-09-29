a819eb418d409f4f437df60a6705a6bd1d3f9a81
roman <mromanek1802@gmail.com>
2022-09-29 20:22:43 +0200

    Cleaning

--- src/main/java/pl/romanek/blog/service/CommentService.java
+++ src/main/java/pl/romanek/blog/service/CommentService.java
@@ -4,7 +4,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import pl.romanek.blog.entity.Comment;
 import pl.romanek.blog.entity.Post;
 import pl.romanek.blog.entity.User;
@@ -28,7 +27,7 @@ public class CommentService {
         return commentRepository.findAll();
     }
 
-    // @Transactional
+    @Transactional
     public void addComment(Comment comment, Integer userId, Integer postId) {
         User user = userService.findUserById(userId).get();
         Post post = postService.findPostById(postId);
