47b36cf102ab64ccbc3c44c2b9a9ea9fb5e36381
roman <mromanek1802@gmail.com>
2022-09-29 20:15:36 +0200

    Cleaning

--- src/main/java/pl/romanek/blog/controller/PostController.java
+++ src/main/java/pl/romanek/blog/controller/PostController.java
@@ -2,8 +2,6 @@ package pl.romanek.blog.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import pl.romanek.blog.entity.Post;
 import pl.romanek.blog.service.PostService;
 
