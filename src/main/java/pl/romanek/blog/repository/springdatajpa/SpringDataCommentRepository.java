b7fda1f20a42ccca3d92a86b0a39d1f5ecf2a0d2
roman <mromanek1802@gmail.com>
2022-10-02 20:27:16 +0200

    Change queries, eliminate N+1

--- src/main/java/pl/romanek/blog/repository/springdatajpa/SpringDataCommentRepository.java
+++ src/main/java/pl/romanek/blog/repository/springdatajpa/SpringDataCommentRepository.java
@@ -1,10 +1,21 @@
 package pl.romanek.blog.repository.springdatajpa;
 
+import java.util.List;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import pl.romanek.blog.entity.Comment;
 import pl.romanek.blog.repository.CommentRepository;
 
 @Profile("spring-data-jpa")
 public interface SpringDataCommentRepository extends JpaRepository<Comment, Integer>, CommentRepository {
+
+    @Override
+    @EntityGraph(type = EntityGraphType.FETCH, attributePaths = { "user", "post" })
+    List<Comment> findAllByPostId(Integer id);
+
+    @Override
+    @EntityGraph(type = EntityGraphType.FETCH, attributePaths = { "user", "post" })
+    List<Comment> findAll();
 }
--- src/main/java/pl/romanek/blog/repository/springdatajpa/SpringDataPostRepository.java
+++ src/main/java/pl/romanek/blog/repository/springdatajpa/SpringDataPostRepository.java
@@ -1,9 +1,12 @@
 package pl.romanek.blog.repository.springdatajpa;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
+
 import pl.romanek.blog.entity.Post;
 import pl.romanek.blog.repository.PostRepository;
 
@@ -12,6 +15,16 @@ public interface SpringDataPostRepository extends JpaRepository<Post, Integer>, 
 
     // Queries not needed - only for educational purpose
     // @Query(value = "SELECT * FROM post WHERE user_id = :id", nativeQuery = true)
-    @Query(value = "SELECT p FROM Post p WHERE p.user.id = :id")
+    // @Query(value = "SELECT p FROM Post p WHERE p.user.id = :id")
+    @Override
+    @EntityGraph(type = EntityGraphType.FETCH, attributePaths = { "user" })
     List<Post> findAllByUserId(Integer id);
+
+    @Override
+    @EntityGraph(type = EntityGraphType.FETCH, attributePaths = { "user" })
+    List<Post> findAll();
+
+    @Override
+    @EntityGraph(type = EntityGraphType.FETCH, attributePaths = { "user" })
+    Optional<Post> findById(Integer id);
 }
