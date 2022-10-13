3e982bdfd7874293f1e37a18503e2aa90c589bfe
roman <mromanek1802@gmail.com>
2022-10-13 20:03:31 +0200

    Add tests for UserController

--- src/main/java/pl/romanek/blog/mapper/UserResponseMapper.java
+++ src/main/java/pl/romanek/blog/mapper/UserResponseMapper.java
@@ -2,11 +2,14 @@ package pl.romanek.blog.mapper;
 
 import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 import pl.romanek.blog.dto.UserResponseDto;
 import pl.romanek.blog.entity.User;
 
 @Mapper(componentModel = "spring")
 public interface UserResponseMapper {
+
+    UserResponseMapper INSTANCE = Mappers.getMapper(UserResponseMapper.class);
 
     UserResponseDto toUserResponseDto(User user);
 
