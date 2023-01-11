c32599eccf10022509d5c8c73162c252efc09394
roman <mromanek1802@gmail.com>
2023-01-11 18:10:10 +0100

    Add DataSourceBuilder

--- src/main/java/pl/romanek/blog/config/AppConfig.java
+++ src/main/java/pl/romanek/blog/config/AppConfig.java
@@ -1,8 +1,25 @@
 package pl.romanek.blog.config;
 
+import javax.sql.DataSource;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class AppConfig {
 
+    @Value("${spring.datasource.password}")
+    String password;
+
+    @Bean
+    DataSource datasource() {
+        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url("jdbc:mysql://localhost:3306/blog");
+        dataSourceBuilder.username("root");
+        dataSourceBuilder.password(password);
+        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
+
+        return dataSourceBuilder.build();
+    }
 }
