73dcd65cc8f46065f3d3d51026612bc36a51cc26
roman <mromanek1802@gmail.com>
2023-01-11 11:50:03 +0100

    Change DB

--- src/main/java/pl/romanek/blog/config/AppConfig.java
+++ src/main/java/pl/romanek/blog/config/AppConfig.java
@@ -1,21 +1,8 @@
 package pl.romanek.blog.config;
 
-import javax.sql.DataSource;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
 
-    @Bean
-    DataSource datasource() {
-        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.url("jdbc:mysql://localhost:3306/blog");
-        dataSourceBuilder.username("root");
-        dataSourceBuilder.password("password");
-        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
-
-        return dataSourceBuilder.build();
-    }
 }
